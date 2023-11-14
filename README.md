1c8639e5d574e3a017569209e1ecc43840c51cbb
py563 <prajwal@vardhaman.org>
2023-11-14 15:48:23 +0530

    models files changed by eslint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wd-201-online-voting
+
 aicte lite capstone project for wd 201 online voting platform
--- models/ovadmin.js
+++ models/ovadmin.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class OVAdmin extends Model {
     /**
@@ -9,18 +7,22 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    // eslint-disable-next-line no-unused-vars
     static associate(models) {
       // define association here
     }
   }
-  OVAdmin.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    emailAddress: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'OVAdmin',
-  });
+  OVAdmin.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      emailAddress: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "OVAdmin",
+    },
+  );
   return OVAdmin;
 };

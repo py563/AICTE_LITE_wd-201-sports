250c1aceab3f4db62eea1e25500c4727ac091be6
py563 <prajwal@vardhaman.org>
2023-11-14 11:48:57 +0530

    add home page

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (request, response) {
   response.render("index");
